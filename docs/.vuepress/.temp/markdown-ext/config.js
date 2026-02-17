998a1c82c42f235c779d1e41352bcc000393f97d
frievoe97 <friedrich.voelkers@me.com>
2026-02-17 15:38:41 +0100

    Stop tracking VuePress generated cache/temp files

--- docs/.vuepress/.temp/markdown-ext/config.js
+++
@@ -1,2 +0,0 @@
-    import "/Users/friedrichvolkers/GIT/matsim-episim/node_modules/@vuepress/plugin-markdown-ext/lib/client/styles/footnote.css"
-    
--- docs/.vuepress/.temp/pages/versions/2025/documentation/example.html.js
+++
@@ -1,3 +0,0 @@
-import comp from "/Users/friedrichvolkers/GIT/matsim-episim/docs/.vuepress/.temp/pages/versions/2025/documentation/example.html.vue"
-const data = JSON.parse("{\"path\":\"/versions/2025/documentation/example.html\",\"title\":\"Example\",\"lang\":\"en-US\",\"frontmatter\":{},\"git\":{},\"filePathRelative\":\"versions/2025/documentation/example.md\"}")
-export { comp, data }
